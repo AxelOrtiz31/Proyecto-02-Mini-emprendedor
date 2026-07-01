580951b1db876835c58401a301520bf551ab4e66
aramis-lopez <145937201+aramis-lopez@users.noreply.github.com>
2026-07-01 16:10:22 -0600

    add

---
+++ mini_emprendedores/app/ia_bot/page.tsx
@@ -0,0 +1,5 @@
+import IABotPage from "@/components/IA_Bot/IA_BotPage";
+
+export default function Page() {
+  return <IABotPage />;
+}
---
+++ mini_emprendedores/app/modules01_06_complete/module06/page.tsx
@@ -0,0 +1,5 @@
+import Module06Page from "@/components/Module_06/Module_06Page";
+
+export default function Page() {
+  return <Module06Page />;
+}
